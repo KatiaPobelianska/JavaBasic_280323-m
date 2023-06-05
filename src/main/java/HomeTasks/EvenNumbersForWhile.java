8d57d35c2d775625b2db65ed4f60b757c41ffe33
cat <alieva957@gmail.com>
2023-06-05 20:05:52 +0200

    homeTask9

---
+++ src/main/java/HomeTasks/EvenNumbersForWhile.java
@@ -0,0 +1,8 @@
+package HomeTasks;
+
+public class EvenNumbersForWhile {
+    public static void main(String[] args) {
+        int i;
+        for (System.out.print((i = 1) + " "); i <= 256; System.out.print((i *= 2) + " "));
+    }
+}
